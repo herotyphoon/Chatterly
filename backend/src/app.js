52e5e7df3e48e5a56ed10683bc5bbcd530bb36e1
HeroTyphoon <pokeaakarsh@gmail.com>
2026-01-02 19:47:25 +0530

    Add Production Deployment Code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,21 @@
+const path = require('path');
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes.js');
 const messageRoutes = require('./routes/message.routes.js');
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    })
+}
 
 module.exports = app;
