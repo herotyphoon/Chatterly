b454dd230156802c9d21d33b721ee6883a5f21cb
HeroTyphoon <pokeaakarsh@gmail.com>
2026-01-03 21:48:54 +0530

    Add Login & Logout Route and Fuctionality

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -33,6 +33,10 @@ const userSchema = new Schema({
     { timestamps: true }
 );
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
 
